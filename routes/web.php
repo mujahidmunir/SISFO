d11259b7296680e3372f701cb4b35fbb5032436e
mujahidmunir <mujahidmunir15@gmail.com>
2020-07-08 15:19:50 +0700

    update root web

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/a', function () {
-    return view('welcome');
-});
+Route::get('/', 'LayoutController@index');
+Route::get('/daftar-guru','LayoutController@list_teacher');
+Route::get('/tambah-guru','LayoutController@add_teacher');
+Route::get('/daftar-siswa','LayoutController@list_student');
+Route::get('/tambah-siswa','LayoutController@add_student');
+Route::get('/daftar-kelas','LayoutController@list_class');
+Route::get('/tambah-kelas','LayoutController@add_class');
+
+
